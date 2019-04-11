12c85bcf8274fc688f4df8b2586f835c76ec2621
Dylan Babbs <dylan.babbs@here.com>
2019-04-11 16:35:38 -0700

    preview hover mode

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const xyzUrl = `https://xyz.api.here.com/hub/spaces/Pqh7dfFY/search?access_token=AJXABoLRYHN488wIHnxheik`;
+const routesUrl = `https://xyz.api.here.com/hub/spaces/Pqh7dfFY/search?access_token=AJXABoLRYHN488wIHnxheik`;
+const neighborhoodsUrl = `https://xyz.api.here.com/hub/spaces/5kwzZmtK/search?access_token=AJXABoLRYHN488wIHnxheik`
+
 const types = ["BIKE LANE", "BUFFERED BIKE LANE", "SHARED-LANE", "PROTECTED BIKE LANE", "OFF-STREET TRAIL", "ACCESS PATH", "NEIGHBORHOOD GREENWAY"]
+
+
 let map;
 async function init() {
-   const tangram = Tangram.leafletLayer({
-      scene: 'scene.yaml',
-      events: {
-         click: onMapClick,
-         hover: onMapHover
-      }
-   })
+   const tangram = Tangram.leafletLayer({ scene: 'scene.yaml' })
+
    const center = [41.81350459907969, -87.7451178431511]
    map = L.map('map', {
       center: center,
@@ -18,8 +17,42 @@ async function init() {
    });
    map.attributionControl.addAttribution('<a href="https://github.com/tangrams/tangram">Tangram</a> | <a href="https://here.xyz">HERE XYZ</a> | <a href="https://www.openstreetmap.org/">OSM</a> | <a href="loading.io">Loading.io</a>');
 
-   const response = await fetch(xyzUrl);
-   const data = await response.json();
+
+   const res = await Promise.all([fetch(routesUrl), fetch(neighborhoodsUrl)]);
+   const [routes, neighborhoods] = await Promise.all(res.map(x => x.json()))
+   console.log(routes);
+   console.log(neighborhoods);
+
+
+   const neighborhoodStyle = {
+      color: '#484848',
+      weight: 0,
+      opacity: 0,
+
+   }
+   L.geoJSON(neighborhoods, {
+      style: neighborhoodStyle,
+      onEachFeature: (feature, layer) => {
+         layer.on({
+            click: handleFeatureClick,
+         })
+         layer.on('mouseover', t => {
+            t.target.setStyle({
+               'fillColor': 'rgba(250,250,250,.5)'
+            });
+            handleFeatureClick(t);
+         });
+         layer.on('mouseout', t => {
+            t.target.setStyle({
+               'fillColor': ''
+            });
+         });
+      }
+   }).addTo(map);
+
+   // map.on('click', (evt) => onMapClick(evt));
+
+
    document.getElementById('loading').style.opacity = '0';
    // tangram.scene.subscribe({
    //    load: (scene) => {
@@ -35,23 +68,20 @@ async function init() {
 
 
 
-   const distances = calculateDistance(data.features, true);
+   const distances = calculateDistance(routes.features, true);
    assignTotals(distances);
 
 
    function assignTotals(n) {
 
-      const keys = Object.keys(n).sort((a,b) => n[b]- n[a])
-      // keys.forEach(key => console.log(`${key}: ${n[key]}`))
+      const keys = Object.keys(n).sort((a, b) => n[b] - n[a]).filter(item => item !== 'total');
+      keys.unshift('total');
       const max = Object.keys(n).reduce((a, b) => n[a] > n[b] ? a : b);
 
       for (let i = 0; i < keys.length; i++) {
-
-         console.log(`${keys[i]}: ${n[keys[i]]}`)
-         // document.getElementById(keys[i]).style.order = i;
          document.getElementById(`${keys[i]}-bar`).style.width = (n[keys[i]] / n[max]) * 100 + '%';
          document.getElementById(`${keys[i]}`).style.top = (i * 40) + 'px';
-         // console.log(document.getElementById(`${keys[i]}`).style.top)
+
          document.getElementById(`${keys[i]}-text`).innerText = '(' + (Math.round(n[keys[i]] * 100) / 100) + ' miles)';
          if (n[keys[i]] === 0) {
             document.getElementById(`${keys[i]}-bar`).style.width = '0%';
@@ -64,67 +94,79 @@ async function init() {
    function onMapHover(evt) {
       if (evt.feature) {
          document.getElementById('map').style.cursor = 'pointer';
-
-         const text = evt.feature.properties.pri_neigh;
-         const popup = L.popup({className: 'custom', closeButton: false})
-         .setLatLng(evt.leaflet_event.latlng)
-         .setContent(text)
-         .openOn(map);
+         // console.
+         // const text = evt.feature.properties.pri_neigh;
+         // const popup = L.popup({
+         //       className: 'custom',
+         //       closeButton: false
+         //    })
+         //    .setLatLng(evt.leaflet_event.latlng)
+         //    .setContent(text)
+         //    .openOn(map);
       } else {
          document.getElementById('map').style.cursor = 'auto';
-         map.closePopup();
+         // map.closePopup();
       }
    }
 
-   async function onMapClick(evt) {
-      if (evt.feature) {
+   function handleFeatureClick(evt) {
+      document.getElementById('neighborhood').innerText = evt.target.feature.properties.pri_neigh;
+      document.getElementById('loading').style.opacity = '1';
 
-         tangram.scene.config.layers.neighborhoods0.filter.pri_neigh = evt.feature.properties.pri_neigh;
-         console.log(tangram.scene.config.layers.neighborhoods0.filter.pri_neigh)
-         const neighborhood = evt.feature.properties.pri_neigh;
 
-         document.getElementById('neighborhood').innerText = neighborhood;
+      const polyGeo = evt.target.feature;
 
-         document.getElementById('loading').style.opacity = '1';
-         const response = await fetch(`https://xyz.api.here.com/hub/spaces/5kwzZmtK/search?access_token=AJXABoLRYHN488wIHnxheik&tags=${evt.feature.id}`);
-         const poly = await response.json();
-         document.getElementById('loading').style.opacity = '0';
+      document.getElementById('loading').style.opacity = '0';
 
-         let cutDistance = 0;
-         const polyGeo = poly.features[0];
-         const temp = []
+      let cutDistance = 0;
+      const temp = []
 
-         data.features.forEach(line => {
-            const points = turf.points(line.geometry.coordinates[0]);
-            const within = turf.pointsWithinPolygon(points, polyGeo);
+      routes.features.forEach(line => {
+         const points = turf.points(line.geometry.coordinates[0]);
+         const within = turf.pointsWithinPolygon(points, polyGeo);
 
-            if (within.features.length > 0) {
-               within.features.forEach(m => m.properties = line.properties)
-               temp.push(within)
+         if (within.features.length > 0) {
+            within.features.forEach(m => m.properties = line.properties)
+            temp.push(within)
+         }
+      })
+      const newLinesForCalc = temp.map(u => {
+         return {
+            "type": "Feature",
+            "properties": u.features[0].properties,
+            "geometry": {
+               "type": "LineString",
+               "coordinates": [u.features.map(f => f.geometry.coordinates)]
+
             }
-         })
-         const newLines = temp.map(u => {
-            const newLineString = {
+         }
+      })
+
+      const newLinesAsFeatureCollection = {
+         type: 'FeatureCollection',
+         features: temp.map(u => {
+            return {
                "type": "Feature",
                "properties": u.features[0].properties,
                "geometry": {
                   "type": "LineString",
-                  "coordinates": [
-                     u.features.map(f => f.geometry.coordinates)
-                  ]
+                  "coordinates": u.features.map(f => f.geometry.coordinates)
+
                }
             }
-            return newLineString
          })
-         const distances = calculateDistance(newLines, true);
-         console.log(distances);
-         assignTotals(distances)
-      } else {
-         const distances = calculateDistance(data.features, true);
-         assignTotals(distances);
-         document.getElementById('neighborhood').innerText = 'City of Chicago';
-
       }
+      console.log(JSON.stringify(newLinesAsFeatureCollection))
+      tangram.scene.setDataSource('_routes', { type: 'GeoJSON', data: newLinesAsFeatureCollection });
+
+      const distances = calculateDistance(newLinesForCalc, true);
+      assignTotals(distances)
+   }
+
+   function handleNonFeatureClick() {
+      const distances = calculateDistance(routes.features, true);
+      assignTotals(distances);
+      document.getElementById('neighborhood').innerText = 'City of Chicago';
    }
 
    function calculateDistance(features, all = false) {
