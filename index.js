8d714019d99fa6b1eb5c3eb0e201cd40932fc7e6
Dylan Babbs <dylan.babbs@here.com>
2019-04-12 11:00:40 -0700

    improve code quality

--- index.js
+++ index.js
@@ -3,63 +3,44 @@ const neighborhoodsUrl = `https://xyz.api.here.com/hub/spaces/5kwzZmtK/search?ac
 
 const types = ["BIKE LANE", "BUFFERED BIKE LANE", "SHARED-LANE", "PROTECTED BIKE LANE", "OFF-STREET TRAIL", "ACCESS PATH", "NEIGHBORHOOD GREENWAY"]
 
-
-let map;
 async function init() {
-   const tangram = Tangram.leafletLayer({ scene: 'scene.yaml' })
+   const tangram = Tangram.leafletLayer({ scene: 'scene.yaml' });
 
-   const center = [41.81350459907969, -87.7451178431511]
-   map = L.map('map', {
+   const center = [41.81350459907969, -87.7451178431511];
+
+   const map = L.map('map', {
       center: center,
       zoom: 8,
       layers: [tangram],
       zoomControl: false
    });
-   map.attributionControl.addAttribution('<a href="https://github.com/tangrams/tangram">Tangram</a> | <a href="https://here.xyz">HERE XYZ</a> | <a href="https://www.openstreetmap.org/">OSM</a> | <a href="loading.io">Loading.io</a>');
-
-
-   // const res = await Promise.all([fetch(routesUrl), fetch(neighborhoodsUrl)]);
-   // const [routes, neighborhoods] = await Promise.all(res.map(x => x.json()))
-
+   map.attributionControl.addAttribution(
+      '<a href="https://github.com/tangrams/tangram">Tangram</a> ' +
+      '| <a href="https://here.xyz">HERE XYZ</a> ' +
+      '| <a href="https://www.openstreetmap.org/">OSM</a> ' +
+      '| <a href="loading.io">Loading.io</a>'
+   );
 
    const [routes, neighborhoods] = await Promise.all([
       fetch(routesUrl).then(x => x.json()),
       fetch(neighborhoodsUrl).then(x => x.json())
    ])
 
-
-   console.log(routes);
-   console.log(neighborhoods);
-
-
-   const neighborhoodStyle = {
-      color: '#484848',
-      weight: 0,
-      opacity: 0,
-   }
    L.geoJSON(neighborhoods, {
-      style: neighborhoodStyle,
+      style: { color: '#484848', weight: 0, opacity: 0},
       onEachFeature: (feature, layer) => {
-         layer.on({
-            click: handleFeatureClick,
-         })
+         layer.on({ click: handleFeatureClick })
          layer.on('mouseover', t => {
-            t.target.setStyle({
-               'fillColor': 'rgba(250,250,250,.5)'
-            });
+            t.target.setStyle({ 'fillColor': 'rgba(250,250,250,.5)'});
          });
          layer.on('mouseout', t => {
-            t.target.setStyle({
-               'fillColor': ''
-            });
+            t.target.setStyle({ 'fillColor': ''});
          });
       }
    }).addTo(map);
 
-   // map.on('click', (evt) => onMapClick(evt));
 
-   document.getElementById('clear').onclick = () => {
-
+   function resetMap() {
       tangram.scene.setDataSource('_routes', {
          type: 'GeoJSON',
          url: 'https://xyz.api.here.com/hub/spaces/Pqh7dfFY/tile/web/{z}_{x}_{y}',
@@ -67,58 +48,35 @@ async function init() {
             access_token: 'AZL4Ab9z2cBFt9d2PQ2hh0k'
          }
       });
-      console.log(tangram.scene.config)
       document.getElementById('clear').style.visibility = 'hidden';
-
-      const distances = calculateDistance(routes.features, true);
+      const distances = calculateDistance(routes.features);
       assignTotals(distances);
       document.getElementById('neighborhood').innerText = 'City of Chicago';
    }
 
+   document.getElementById('clear').onclick = () => resetMap();
 
    document.getElementById('clip').onchange = () => {
       if (!document.getElementById('clip').checked) {
-         tangram.scene.setDataSource('_routes', {
-            type: 'GeoJSON',
-            url: 'https://xyz.api.here.com/hub/spaces/Pqh7dfFY/tile/web/{z}_{x}_{y}',
-            url_params: {
-               access_token: 'AZL4Ab9z2cBFt9d2PQ2hh0k'
-            }
-         });
-         document.getElementById('clear').style.visibility = 'hidden';
-         const distances = calculateDistance(routes.features, true);
-         assignTotals(distances);
-         document.getElementById('neighborhood').innerText = 'City of Chicago';
-
+         resetMap();
       }
    }
 
 
-   document.getElementById('loading').style.opacity = '0';
-   // tangram.scene.subscribe({
-   //    load: (scene) => {
-   //       console.log('h')
-   //       document.getElementById('loading').style.opacity = '0';
-   //    }
-   // })
 
    map.flyTo(center, 11, {
       animate: true,
       duration: 0.5
    })
 
-   console.log(
-      document.querySelector('.bar-container').clientHeight
-   )
+   document.getElementById('loading').style.opacity = '0';
 
-
-
-   const distances = calculateDistance(routes.features, true);
-   assignTotals(distances);
+   assignTotals(
+      calculateDistance(routes.features)
+   );
 
 
    function assignTotals(n) {
-
       const keys = Object.keys(n).sort((a, b) => n[b] - n[a]).filter(item => item !== 'total');
       keys.unshift('total');
       const max = Object.keys(n).reduce((a, b) => n[a] > n[b] ? a : b);
@@ -128,37 +86,18 @@ async function init() {
          document.getElementById('total-text').style.color = '#C7C7C7';
       } else {
          document.getElementById('total-bar').style.color = 'black';
-         document.getElementById('total-text').style.color = '#434343'
+         document.getElementById('total-text').style.color = '#434343';
       }
 
       for (let i = 0; i < keys.length; i++) {
          document.getElementById(`${keys[i]}-bar`).style.width = (n[keys[i]] / n[max]) * 100 + '%';
          document.getElementById(`${keys[i]}`).style.top = (i * 40) + 'px';
-
          document.getElementById(`${keys[i]}-text`).innerText = '(' + (Math.round(n[keys[i]] * 100) / 100) + ' miles)';
+
          if (n[keys[i]] === 0) {
             document.getElementById(`${keys[i]}-bar`).style.width = '0%';
-
             document.getElementById(`${keys[i]}-text`).innerText = '(0 miles)';
          }
-      }
-   }
-
-   function onMapHover(evt) {
-      if (evt.feature) {
-         document.getElementById('map').style.cursor = 'pointer';
-         // console.
-         // const text = evt.feature.properties.pri_neigh;
-         // const popup = L.popup({
-         //       className: 'custom',
-         //       closeButton: false
-         //    })
-         //    .setLatLng(evt.leaflet_event.latlng)
-         //    .setContent(text)
-         //    .openOn(map);
-      } else {
-         document.getElementById('map').style.cursor = 'auto';
-         // map.closePopup();
       }
    }
 
@@ -166,23 +105,23 @@ async function init() {
       document.getElementById('neighborhood').innerText = 'Neighborhood: ' + evt.target.feature.properties.pri_neigh;
       document.getElementById('loading').style.opacity = '1';
 
-
       const polyGeo = evt.target.feature;
 
       document.getElementById('loading').style.opacity = '0';
 
       let cutDistance = 0;
-      const temp = []
+      const temp = [];
 
       routes.features.forEach(line => {
          const points = turf.points(line.geometry.coordinates[0]);
          const within = turf.pointsWithinPolygon(points, polyGeo);
 
          if (within.features.length > 0) {
-            within.features.forEach(m => m.properties = line.properties)
-            temp.push(within)
+            within.features.forEach(m => m.properties = line.properties);
+            temp.push(within);
          }
       })
+
       const newLinesForCalc = temp.map(u => {
          return {
             "type": "Feature",
@@ -193,7 +132,7 @@ async function init() {
 
             }
          }
-      })
+      });
 
       const newLinesAsFeatureCollection = {
          type: 'FeatureCollection',
@@ -204,64 +143,43 @@ async function init() {
                "geometry": {
                   "type": "LineString",
                   "coordinates": u.features.map(f => f.geometry.coordinates)
-
                }
             }
          })
-      }
+      };
 
       if (document.getElementById('clip').checked){
          tangram.scene.setDataSource('_routes', { type: 'GeoJSON', data: newLinesAsFeatureCollection });
-
       }
       document.getElementById('clear').style.visibility = 'visible';
 
-
-
-      const distances = calculateDistance(newLinesForCalc, true);
-      assignTotals(distances)
+      assignTotals(
+         calculateDistance(newLinesForCalc)
+      )
    }
 
-   function calculateDistance(features, all = false) {
-      let distance = 0;
+   function calculateDistance(features) {
       const distances = {};
-
       types.forEach(q => distances[q] = 0);
       distances['total'] = 0;
 
       for (let z = 0; z < features.length; z++) {
          const feature = features[z];
          const geometry = feature.geometry.coordinates[0];
-         const t = feature.properties.bikeroute
+         const t = feature.properties.bikeroute;
          for (let i = 0; i < geometry.length - 1; i++) {
-            if (all) {
-               distances[t] += turf.distance(
-                  turf.point(geometry[i]),
-                  turf.point(geometry[i + 1]), {
-                     units: 'miles'
-                  }
-               )
-               distances['total'] += turf.distance(
-                  turf.point(geometry[i]),
-                  turf.point(geometry[i + 1]), {
-                     units: 'miles'
-                  }
-               )
-            } else {
-               distance += turf.distance(
-                  turf.point(geometry[i]),
-                  turf.point(geometry[i + 1]), {
-                     units: 'miles'
-                  }
-               )
-            }
+            const e = turf.distance(
+               turf.point(geometry[i]),
+               turf.point(geometry[i + 1]), {
+                  units: 'miles'
+               }
+            )
+            distances[t] += e
+            distances['total'] += e
          }
       }
-      return all ? distances : distance;
+      return distances;
    }
-
 }
-
-
 
 init();
